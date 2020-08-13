f54c34d3531922d927525839163963c02eebb7a9
SyedIftekharuddin <syedy636@gmail.com>
2020-08-13 14:36:18 +0200

    Added some unit tests and some integration test

--- src/test/java/com/app/utb/recipeproject/RecipeprojectApplicationTests.java
+++ src/test/java/com/app/utb/recipeproject/RecipeprojectApplicationTests.java
@@ -3,6 +3,7 @@ package com.app.utb.recipeproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class RecipeprojectApplicationTests {
 
